60e6b4a6d66da677aa8eb90e3a2bed8d3ec150c6
Tamás Csaba <tamcsaba@gmail.com>
2022-10-04 13:49:49 +0200

    feat(cards): use only static render :computer:

--- src/components/cards/cards.component.ts
+++ src/components/cards/cards.component.ts
@@ -1,22 +1,17 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
-import { PlayerService } from '../../services/player.service';
-import { RoomService } from '../../services/room.service';
 
 @Component({
     selector: 'app-cards',
     templateUrl: './cards.component.html',
     styleUrls: ['./cards.component.scss'],
     imports: [NgFor],
-    providers: [PlayerService, RoomService],
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardsComponent implements OnInit {
     public readonly cards = [0, 1, 2, 3, 5, 8, 13, 21];
     constructor(
-        private readonly playerService: PlayerService,
-        private readonly roomService: RoomService
     ) { }
 
     async ngOnInit() {
